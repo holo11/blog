f0c9ad5625d677dee3e174c10e425c6f914fab24
holo11 <holo1122@gmail.com>
2023-12-14 17:32:16 +0900

    commit msg

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   
   # GET /posts or /posts.json
   def index
+    
     @posts = Post.all
     respond_to do |format|
       format.html # index.html.erb
